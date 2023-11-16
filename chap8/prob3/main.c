a8b35ccaab8e35049a029985ddff3a8c277bc4a7
muneo1008 <mkmk100872@naver.com>
2023-11-16 14:58:27 +0900

    chap8

---
+++ chap8/prob3/main.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char *argv[]){
+	char *ptr;
+	
+	ptr = getenv("HOME");
+	printf("HOME = %s \n",ptr);
+
+	ptr = getenv("SHEEL");
+	printf("SHELL = %s \n",ptr);
+
+	ptr = getenv("PATH");
+	printf("PATH = %s \n",ptr);
+	exit(0);
+}
