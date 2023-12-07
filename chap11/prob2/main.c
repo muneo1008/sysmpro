aaaf147fa4fb4d6ae7959a56e984751149f53c4f
muneo1008 <mkmk100872@naver.com>
2023-12-07 14:42:27 +0900

    chap11

---
+++ chap11/prob2/main.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <signal.h>
+#include <stdlib.h>
+void alarmHandler();
+
+int main(){
+	signal(SIGALRM, alarmHandler);
+	alarm(5);
+	printf("무한 루프\n");
+	while(1){
+		sleep(1);
+		printf("1초 경과\n");
+	}
+	printf("실행되지 않음\n");
+}
+
+void alarmHandler(int signo){
+	printf("일어나세요\n");
+	exit(0);
+}
